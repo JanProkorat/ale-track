23dabc6318137818ed2e0fd603c8ac9cb59d9804
jan <prokoratJ@gmail.com>
2025-08-24 19:13:59 +0200

    feat: Add render configuration

--- api/AleTrack/AleTrack/obj/Debug/net8.0/AleTrack.AssemblyInfo.cs
+++ api/AleTrack/AleTrack/obj/Debug/net8.0/AleTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AleTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b301c0b9273c4d125d1d963ca4da2bc58111b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b763e22dd9e7303f37f09410ab32af251b9c1aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("AleTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AleTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
