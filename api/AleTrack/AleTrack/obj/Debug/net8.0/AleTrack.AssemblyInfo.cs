b5533431849cb12978b40c75eaa3481fded11b28
jan <prokoratJ@gmail.com>
2025-08-24 21:21:35 +0200

    feat: add logs for debugging

--- api/AleTrack/AleTrack/obj/Debug/net8.0/AleTrack.AssemblyInfo.cs
+++ api/AleTrack/AleTrack/obj/Debug/net8.0/AleTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AleTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+754175754048a0b89fd66ce21ef7aefab9e21944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3dc1c77c6f5850bbdeba074e37e95325a13a7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AleTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AleTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
