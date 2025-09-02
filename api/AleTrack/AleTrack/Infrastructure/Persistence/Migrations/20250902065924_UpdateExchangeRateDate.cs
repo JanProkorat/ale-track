1143d41fd33c76aae65a6e4c95ae3c577e179ec3
jan <prokoratJ@gmail.com>
2025-09-02 09:04:57 +0200

    feat: #18 update date on entity

---
+++ api/AleTrack/AleTrack/Infrastructure/Persistence/Migrations/20250902065924_UpdateExchangeRateDate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AleTrack.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateExchangeRateDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "updated_date",
+                table: "exchange_rates",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "updated_date",
+                table: "exchange_rates",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
