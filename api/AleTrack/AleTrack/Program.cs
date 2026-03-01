60ac248d47bcba20657fdefa5d2a7b300527f24e
Jan Prokorát <prokoratJ@gmail.com>
2026-03-01 14:21:02 +0100

    feat: allow CORS for development application host

--- api/AleTrack/AleTrack/Program.cs
+++ api/AleTrack/AleTrack/Program.cs
@@ -88,6 +88,11 @@ try
     {
         options.AddPolicy("AllowFrontend", policy =>
         {
+            policy.WithOrigins("https://dev-ale-track.netlify.app")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+            
             policy.WithOrigins("https://ale-track.netlify.app")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
